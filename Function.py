fb909394deb85eb66cc19f4a3e4864e85786703d
Adwait Pimple <141001322+Adwaitp-cloud@users.noreply.github.com>
2026-02-21 07:53:07 +0530

    Eliminate unused double function and prints
    Removed the double function and its related print statements to streamline the code.

--- Function.py
+++ Function.py
@@ -86,20 +86,3 @@ def calculate_area(height,width):
 
 room_area = calculate_area(12 ,10)
 print(f"Room size: {room_area} sq ft")
-
-
-def double(number):
-    return number * 2
-
-# Store in variable
-result = double(5)
-
-# Use in expressions
-total = double(5) + double(3)  # 10 + 6 = 16
-
-# Pass to other functions
-print(double(10))  # 20
-
-# Use in conditions
-if double(7) > 10:
-    print("Big number!")
