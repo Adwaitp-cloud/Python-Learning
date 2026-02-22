8fe41f116bd26ed282aa3246eab2d69f81fbc1d5
Adwait Pimple <thinkcreative891@gmail.com>
2026-02-22 21:40:30 +0530

    Knowing the python library

--- libraries-ex.py
+++ libraries-ex.py
@@ -1,7 +1,7 @@
 # There are two types of import types
 
 # First
-# imports compelete package
+# imports compelete library
 import math
 
 square_root = math.sqrt(16)
@@ -10,14 +10,19 @@ print(math.pi)
 
 # Second
 
-# imports specific items from the packages
+# imports specific items from the library
 from math import sqrt, pi
 
 sqrt(16)
 pi
 
-# This packages pick random values
+# This library pick random values
 import random
 number = random.randint(1, 10)
 choice = random.choice(["apple", "banana", "orange"])
 
+
+# Date and time library
+import datetime
+today = datetime.date.today()
+print(today)
